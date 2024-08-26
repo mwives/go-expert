a828e36256c9bc369d2f7a6a5864ebbf3b98e01c
Ives M. W <ivesmw@gmail.com>
2024-08-25 21:09:16 -0300

    fix: use float64 in product price

--- apis/internal/entity/product_test.go
+++ apis/internal/entity/product_test.go
@@ -8,12 +8,12 @@ import (
 )
 
 func TestProduct_NewProduct(t *testing.T) {
-	p, err := NewProduct("Product 1", 10)
+	p, err := NewProduct("Product 1", 10.0)
 	assert.Nil(t, err)
 	assert.NotNil(t, p)
 	assert.NotEmpty(t, p.ID)
 	assert.Equal(t, "Product 1", p.Name)
-	assert.Equal(t, 10, p.Price)
+	assert.Equal(t, 10.0, p.Price)
 }
 
 func TestProduct_Validate_ErrIDRequired(t *testing.T) {
