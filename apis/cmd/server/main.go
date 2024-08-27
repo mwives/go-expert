9eb3cc381e9fd9f26261221bfa10923ebb52f82b
Ives M. W <ivesmw@gmail.com>
2024-08-26 23:31:36 -0300

    feat: add HTTP handler for CreateProduct

--- apis/cmd/server/main.go
+++ apis/cmd/server/main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
+
 	"github.com/mwives/go-expert/apis/configs"
 	"github.com/mwives/go-expert/apis/internal/entity"
+	"github.com/mwives/go-expert/apis/internal/infra/database"
+	"github.com/mwives/go-expert/apis/internal/infra/webserver/handlers"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
 func main() {
-	_, err := configs.LoadConfig(".")
+	config, err := configs.LoadConfig(".")
 	if err != nil {
 		panic(err)
 	}
@@ -17,4 +22,12 @@ func main() {
 		panic(err)
 	}
 	db.AutoMigrate(&entity.Product{}, &entity.User{})
+
+	productDB := database.NewProduct(db)
+	productHandler := handlers.NewProductHandler(productDB)
+
+	http.HandleFunc("/products", productHandler.CreateProduct)
+
+	fmt.Printf("Starting server on port %s\n", config.WebServerPort)
+	http.ListenAndServe(fmt.Sprintf(":%s", config.WebServerPort), nil)
 }
