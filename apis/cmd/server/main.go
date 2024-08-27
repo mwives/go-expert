79ab5c32e8e3848c63adfea6de3c235485a0cc1a
Ives M. W <ivesmw@gmail.com>
2024-08-26 23:31:10 -0300

    feat: add database initialization and migration

--- apis/cmd/server/main.go
+++ apis/cmd/server/main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"github.com/mwives/go-expert/apis/configs"
+	"github.com/mwives/go-expert/apis/internal/entity"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
 func main() {
-	// ...
+	_, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&entity.Product{}, &entity.User{})
 }
