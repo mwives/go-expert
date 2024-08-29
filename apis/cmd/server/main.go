e0fc8c47297c6423995427f593253a62e4ed81ea
Ives M. W <ivesmw@gmail.com>
2024-08-29 20:48:48 -0300

    feat: add JWT authentication to product endpoints

--- apis/cmd/server/main.go
+++ apis/cmd/server/main.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/jwtauth"
 	"github.com/mwives/go-expert/apis/configs"
 	"github.com/mwives/go-expert/apis/internal/entity"
 	"github.com/mwives/go-expert/apis/internal/infra/database"
@@ -33,11 +34,18 @@ func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 
-	r.Post("/products", productHandler.CreateProduct)
-	r.Get("/products", productHandler.GetProducts)
-	r.Get("/products/{id}", productHandler.GetProduct)
-	r.Put("/products/{id}", productHandler.UpdateProduct)
-	r.Delete("/products/{id}", productHandler.DeleteProduct)
+	r.Route("/products", func(r chi.Router) {
+		// Add the JWTAuth middleware to the router
+		r.Use(jwtauth.Verifier(config.TokenAuth))
+		// Verify the token with the JWTAuth middleware
+		r.Use(jwtauth.Authenticator)
+
+		r.Post("/", productHandler.CreateProduct)
+		r.Get("/", productHandler.GetProducts)
+		r.Get("/{id}", productHandler.GetProduct)
+		r.Put("/{id}", productHandler.UpdateProduct)
+		r.Delete("/{id}", productHandler.DeleteProduct)
+	})
 
 	r.Post("/users", userHandler.CreateUser)
 	r.Post("/users/auth", userHandler.GetJWT)
