8f0fdc25775b1c6e533463fead0c9db3a7ec4ae8
Ives M. W <ivesmw@gmail.com>
2024-09-08 18:04:58 -0300

    feat: add RabbitMQ consumer and producer functionality

---
+++ events/cmd/consumer/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/mwives/go-expert/events/pkg/rabbitmq"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func main() {
+	ch, err := rabbitmq.OpenChannel()
+	if err != nil {
+		panic(err)
+	}
+	defer ch.Close()
+
+	msgs := make(chan amqp.Delivery)
+
+	go rabbitmq.Consume(ch, msgs, "myqueue")
+	for msg := range msgs {
+		fmt.Println(string(msg.Body))
+		msg.Ack(false)
+	}
+}
---
+++ events/cmd/producer/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/mwives/go-expert/events/pkg/rabbitmq"
+)
+
+func main() {
+	ch, err := rabbitmq.OpenChannel()
+	if err != nil {
+		panic(err)
+	}
+	defer ch.Close()
+
+	rabbitmq.Publish(ch, "Hello World!", "amq.direct")
+}
