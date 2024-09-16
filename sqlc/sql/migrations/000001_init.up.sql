b83c49d6ec3d04333fc5e1baa49002a5f7aa85ec
Ives M. W <ivesmw@gmail.com>
2024-09-16 16:07:11 -0300

    feat: add SQL migration files with golang-migrate

---
+++ sqlc/sql/migrations/000001_init.up.sql
@@ -0,0 +1,14 @@
+CREATE TABLE categories (
+  id VARCHAR(36) NOT NULL PRIMARY KEY,
+  name TEXT NOT NULL,
+  description TEXT
+);
+
+CREATE TABLE courses (
+  id VARCHAR(36) NOT NULL PRIMARY KEY,
+  category_id VARCHAR(36) NOT NULL,
+  name TEXT NOT NULL,
+  description TEXT,
+  price decimal(10, 2) NOT NULL,
+  FOREIGN KEY (category_id) REFERENCES categories(id)
+);
